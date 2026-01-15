48eb0e57fca00ba644ab7428879a87618c2e2b6c
Gio <gio.ponson@outlook.com>
2026-01-15 02:37:40 +0100

    Update API URL for production

--- frontend/config.js
+++ frontend/config.js
@@ -2,5 +2,5 @@
 const CONFIG = {
     API_BASE_URL: window.location.hostname === 'localhost' || window.location.hostname === '127.0.0.1'
         ? 'http://localhost:5000'  // Development
-        : 'azure'  // Production (need to update)
+        : 'https://trivia-api-quad-giepie-cbfjg6cxcyhcgnca.westeurope-01.azurewebsites.net'  // Production
 };
